9795c00d63ae015e901cafd95ae917d442ad497f
HyuHyu <dangnguyenduchuy@gmail.com>
2024-09-23 21:49:01 +0700

    [feat] add, delete function

--- src/App.js
+++ src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+function Header(props) {
+	return (
+		<h1>{props.title}</h1>
+	)
+}
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [todo, setTodo] = useState([]);
+	const [newTodo, setNewTodo] = useState('');
+
+	function handleCreate() {
+		setTodo([...todo, newTodo])
+		setNewTodo('')
+	}
+
+	function handleDelete(delItem) {
+		let deleted = false
+		setTodo(todo.filter(e => {
+			if (!deleted && (e === delItem)) {
+				deleted = true
+				return false
+			}
+			return true
+		}));
+	}
+
+	return (
+		<div style={{ padding: 20 }}>
+			<Header title="Todo List" />
+			<form className="d-flex flex-row align-items-center">
+				<label htmlFor="name" className="form-label">Title</label>
+				<input
+					style={{ margin: 10, width: 300 }}
+					className="form-control"
+					id="name"
+					value={newTodo}
+					onChange={e => setNewTodo(e.target.value)}
+				/>
+				<button onClick={handleCreate} type="button" className="btn btn-primary">Create</button>
+			</form>
+			<ul>
+				{todo.length ? todo.map((e, index) => {
+					return (
+						<li key={index}>
+							<p style={{ display: "inline", marginRight: 10 }}>{e}</p>
+							<button onClick={() => handleDelete(e)} className="btn-close"></button>
+						</li>
+					)
+				}) : 'Nothing to do'}
+			</ul>
+		</div>
+	)
 }
+
+
+// function App() {
+
+// 	const [name, setName] = useState('') 
+
+// 	console.log(name)
+
+// 	return (
+// 		<div>
+// 			<input 
+// 				value={name}
+// 				onChange={e => setName(e.target.value)} 
+// 			/> 
+// 			<button onClick={() => setName('test')}>Change</button>
+// 		</div>
+// 	)
+// }
+
 
 export default App;
